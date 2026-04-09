ceb26efbc2348ea7ebef0929a8047588a603e037
WeeklyFurball <landenhibshman@gmail.com>
2026-04-09 08:57:30 -0400

    Godbolt and Rock items are in game, crafting recipe for Godbolt is functional. Item functionality still needs to be implemented.

--- src/main/java/com/weeklyfurball/throwit/item/ModItems.java
+++ src/main/java/com/weeklyfurball/throwit/item/ModItems.java
@@ -2,6 +2,7 @@ package com.weeklyfurball.throwit.item;
 
 import com.weeklyfurball.throwit.ThrowIt;
 import com.weeklyfurball.throwit.item.custom.GodboltItem;
+import com.weeklyfurball.throwit.item.custom.RockItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -13,6 +14,7 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, ThrowIt.MOD_ID);
 
     public static final RegistryObject<Item> GODBOLT = ITEMS.register("godbolt", () -> new GodboltItem(new Item.Properties().durability(1000)));
+    public static final RegistryObject<Item> ROCK = ITEMS.register("rock", () -> new RockItem(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
---
+++ src/main/java/com/weeklyfurball/throwit/item/custom/RockItem.java
@@ -0,0 +1,11 @@
+package com.weeklyfurball.throwit.item.custom;
+
+import net.minecraft.world.item.Item;
+
+public class RockItem extends Item {
+
+    public RockItem(Properties pProperties) {
+        super(pProperties);
+    }
+
+}
