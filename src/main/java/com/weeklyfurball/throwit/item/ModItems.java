b0fa9faa21352d4d0f891ef2cfb7bfbc9398b356
WeeklyFurball <landenhibshman@gmail.com>
2026-04-08 22:09:46 -0400

    Godbolt is in game, further work needed

--- src/main/java/com/weeklyfurball/throwit/item/ModItems.java
+++ src/main/java/com/weeklyfurball/throwit/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.weeklyfurball.throwit.item;
 
 import com.weeklyfurball.throwit.ThrowIt;
+import com.weeklyfurball.throwit.item.custom.GodboltItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -11,7 +12,7 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, ThrowIt.MOD_ID);
 
-    public static final RegistryObject<Item> GODBOLT = ITEMS.register("godbolt", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> GODBOLT = ITEMS.register("godbolt", () -> new GodboltItem(new Item.Properties().durability(1000)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
